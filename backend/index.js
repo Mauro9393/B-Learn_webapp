20e6b1e07f15e40513cc894c0322a2169d354095
Mauro9393 <m.dicarlo@baberlearning.fr>
2025-05-23 11:32:51 +0200

    fix: aggiunto CORS per frontend

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const authRoutes = require('./src/routes/authRoutes');
 const userRoutes = require('./src/routes/userRoutes');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors({
+    origin: 'http://163.172.159.116',
+    credentials: true
+}));
 app.use(express.json());
 app.use('/auth', authRoutes);
 app.use('/api', userRoutes);
