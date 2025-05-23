5b8ee85d1692b0b99801ecf3063e6b901952b4e4
Mauro9393 <m.dicarlo@baberlearning.fr>
2025-05-23 10:57:14 +0200

    removing crypt to frontend

--- frontend/src/Login.tsx
+++ frontend/src/Login.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import './assets/css/login.css';
 import { useNavigate } from 'react-router-dom';
-import bcrypt from 'bcryptjs';
 
 function Login() {
   const navigate = useNavigate();
